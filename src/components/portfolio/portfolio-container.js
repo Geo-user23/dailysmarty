5b68ed9c8e66f321a1b0b074d0c2637d8f4b1cbc
Jovani Rodriguez <Jovanirodriguez220@gmail.com>
2022-10-08 15:22:10 -0700

    first prop

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -2,12 +2,23 @@ import React, {Component} from "react";
 
 import PortfolioItem from "./portfolio-item";
 export default class PortfolioContainer extends Component {
+    constructor() {
+        super();
+    }
+
+    portfolioItems() {
+        const data = ["quip", "eventbright", "sonic"];
+
+        return data.map(item => {
+            return <PortfolioItem title ={item} />;
+        })
+    }
     render() {
         return (
             <div>
                 <h2>Porfolio items go here...</h2>
 
-                <PortfolioItem/>
+                {this.portfolioItems()}
             </div>
         )
     }
