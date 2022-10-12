131430d4928a8fc5ca764f331e17b04011482b5a
Jovani Rodriguez <Jovanirodriguez220@gmail.com>
2022-10-11 20:14:05 -0700

    key prop and data setup

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import moment from "moment";
-import axios from "axios";
 import {
   BrowserRouter as Router,
   Switch,
@@ -17,26 +16,9 @@ import Blog from "./pages/blog";
 import PortfolioDetail from "./portfolio/portfolio-detail";
 import NoMatch from "./pages/no-match"
 export default class App extends Component {
-    constructor() {
-      super();
-
-      this.getPortfolioItems = this.getPortfolioItems.bind(this);
-    }
-    getPortfolioItems() {
-    axios
-    .get('https://jovanirodriguez.devcamp.space/portfolio/portfolio_items')
-  .then(response => {
-    // handle success
-    console.log(response);
-  })
-  .catch(error => {
-    // handle error
-    console.log(error);
-  });
-  }
+    
 
   render() {
-    this.getPortfolioItems();
     return (
       <div className='app'>
         <Router>
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import axios from "axios";
 
 import PortfolioItem from "./portfolio-item";
 export default class PortfolioContainer extends Component {
@@ -8,15 +9,11 @@ export default class PortfolioContainer extends Component {
         this.state = {
             pageTitle: "welcome to my portfolio",
             isLoading: false,
-            data: [
-                {title: "quip", category: "eCommerce", slug: 'quip'},
-                {title : "eventbrite", category: "Scheduling", slug:'eventbrite'},
-                {title: "Ministry Safe", category: "Enterprise", slug:'ministry-safe'},
-                {title : "SwingAway", category: "eCommerce", slug: 'swingaway'}
-            ]
+            data: []
         };
 
         this.handleFilter = this.handleFilter.bind(this);
+        this.getPortfolioItems = this.getPortfolioItems.bind(this);
 
     }
 
@@ -28,17 +25,36 @@ export default class PortfolioContainer extends Component {
        })
     }
 
+    getPortfolioItems() {
+        axios
+        .get('https://jovanirodriguez.devcamp.space/portfolio/portfolio_items')
+      .then(response => {
+        // handle success
+        console.log(response);
+        this.setState({
+            data: response.data.portfolio_items
+        });
+      })
+      .catch(error => {
+        // handle error
+        console.log(error);
+      });
+      }
+
     portfolioItems() {
         return this.state.data.map(item => {
-            return <PortfolioItem title ={item.title} url={"google.com"} slug={item.slug}/>;
+            return <PortfolioItem key={item.id} title ={item.name} url={item.url} slug={item.id}/>;
         })
     }
 
-    
+    componentDidMount() {
+        this.getPortfolioItems();
+    }
     render() {
         if (this.state.isLoading) {
             return <div>Loading...</div>
         }
+
         return (
             <div>
                 <h2>{this.state.pageTitle}.</h2>
