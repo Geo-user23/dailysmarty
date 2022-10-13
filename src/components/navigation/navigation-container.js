1d83c27ebb86f971ac5c29aa2c42046bff076760
Jovani Rodriguez <Jovanirodriguez220@gmail.com>
2022-10-12 20:35:08 -0700

    variables file in scss

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -11,11 +11,21 @@ export default class Navigationcomponent extends Component {
         return (
             <div className="nav-wrapper">
                 <div className="left-side">
+                    <div className="nav-link-wrapper">
                 <NavLink exact to="/" activeClassName="nav-link-active"> Home </NavLink>
+                    </div>
+
+                    <div className="nav-link-wrapper">
                 <NavLink  to="/about-me" activeClassName="nav-link-active">About</NavLink>
+                    </div>
+
+                    <div className="nav-link-wrapper">
                 <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
+                     </div>
+
+                     <div className="nav-link-wrapper">
                 <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
-                { false ? <button>Add Blog</button> : null}
+                     </div>
                 </div>
 
                 <div className="right-side">
