82d7d4fa3cbe41b024f139e843404142dbe6309f
Jovani Rodriguez <Jovanirodriguez220@gmail.com>
2022-10-13 21:23:59 -0700

    comeback add image and implemented styles for login

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,11 +1,19 @@
-import React, { Component, component } from 'react';
+import React, { Component } from "react";
+
 
 export default class Auth extends Component {
-    render() {
-        return (
-         <div>
-            auth......
-         </div>
-        );
-    }
+  render() {
+    return (
+      <div className="auth-page-wrapper">
+        <div
+          className="left-column"
+        />
+
+        <div className="right-column">
+          <h1>Login component goes here...</h1>
+        </div>
+      </div>
+    );
+  }
 }
+
