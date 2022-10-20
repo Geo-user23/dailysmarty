9b2e1088ab497b41ecd6d2315674efd73ffd19fd
Jovani Rodriguez <Jovanirodriguez220@gmail.com>
2022-10-19 20:03:48 -0700

    scroll and its three attributes scrolltop, offsetheight, and documentelement

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -17,8 +17,13 @@ class Blog extends Component {
 
   activateInfiniteScroll() {
     window.onscroll = () => {
-
-    }
+      if (
+        window.innerHeight + document.documentElement.scrollTop ===
+        document.documentElement.offsetHeight
+      ) {
+        console.log("get more posts");
+      }
+    };
   }
 
   getBlogItems() {
@@ -45,13 +50,11 @@ class Blog extends Component {
       return <BlogItem key={blogItem.id} blogItem={blogItem} />;
     });
 
-     return (
-    <div className="blog-container">
-        <div className="content-container">
-        {blogRecords}
-          </div>
-          </div>
-     );
+    return (
+      <div className="blog-container">
+        <div className="content-container">{blogRecords}</div>
+      </div>
+    );
   }
 }
 
