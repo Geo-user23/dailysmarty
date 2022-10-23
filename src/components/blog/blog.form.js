938a3565591b1401f00bfaaee7fbaab892746923
Jovani Rodriguez <Jovanirodriguez220@gmail.com>
2022-10-23 13:06:51 -0700

    fixing state on unmounted component

--- src/components/blog/blog.form.js
+++ src/components/blog/blog.form.js
@@ -42,14 +42,21 @@ export default class BlogForm extends Component {
         { withCredentials: true }
       )
       .then(response => {
+        this.setState({
+          title: "",
+          blog_status: "",
+          content: ""
+        });
+        
         this.props.handleSuccessfullFormSubmission(
           response.data.portfolio_blog
         );
 
-        this.setState({
-          title: "",
-          blog_status: ""
-        });
+        this.props.handleSuccessfullFormSubmission(
+          response.data.portfolio_blog
+        );
+
+        
       })
       .catch(error => {
         console.log("handleSubmit for blog error", error);
