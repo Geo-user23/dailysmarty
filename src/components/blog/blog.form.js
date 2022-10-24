d61ca971f105c68772169a3b8e3d117b3835c229
Jovani Rodriguez <Jovanirodriguez220@gmail.com>
2022-10-23 21:36:10 -0700

    rendering image on dropzone

--- src/components/blog/blog.form.js
+++ src/components/blog/blog.form.js
@@ -29,13 +29,13 @@ export default class BlogForm extends Component {
     this.featuredImageRef = React.createRef();
   }
 
-  componentWillMount(){
-    if(this.props.editMode) {
+  componentWillMount() {
+    if (this.props.editMode) {
       this.setState({
-        id:this.props.blog.id,
+        id: this.props.blog.id,
         title: this.props.blog.title,
         status: this.props.blog.status
-      })
+      });
     }
   }
 
@@ -142,19 +142,33 @@ export default class BlogForm extends Component {
           <RichTextEditor
             handleRichTextEditorChange={this.handleRichTextEditorChange}
             editMode={this.props.editMode}
-            contentToEdit={this.props.editMode && this.props.blog.content ? this.props.blog.content : null}
+            contentToEdit={
+              this.props.editMode && this.props.blog.content
+                ? this.props.blog.content
+                : null
+            }
           />
         </div>
 
         <div className="image-uploaders">
-          <DropzoneComponent
-            ref={this.featuredImageRef}
-            config={this.componentConfig()}
-            djsConfig={this.djsConfig()}
-            eventHandlers={this.handleFeaturedImageDrop()}
-          >
-            <div className="dz-message">Featured Image</div>
-          </DropzoneComponent>
+          {this.props.editMode && this.props.blog.featured_image_url ? (
+            <div className="portfolio-manager-image-wrapper">
+              <img src={this.props.blog.featured_image_url} />
+
+              <div className="image-removal-link">
+                <a>Remove file</a>
+              </div>
+            </div>
+          ) : (
+            <DropzoneComponent
+              ref={this.featuredImageRef}
+              config={this.componentConfig()}
+              djsConfig={this.djsConfig()}
+              eventHandlers={this.handleFeaturedImageDrop()}
+            >
+              <div className="dz-message">Featured Image</div>
+            </DropzoneComponent>
+          )}
         </div>
 
         <button className="btn">Save</button>
