bbf00d65a40a5ebc02f8b0b099901df20214301e
Jovani Rodriguez <Jovanirodriguez220@gmail.com>
2022-10-24 20:50:57 -0700

    working with filter

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 export default function() {
-    return (
-        <div>
-            Contact
+  return (
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        
+      />
+      <div className="right-column">
+        <div className="contact-bullet-points">
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="phone" />
+            </div>
+
+            <div className="text">--</div>
+          </div>
+
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="envelope" />
+            </div>
+
+            <div className="text">jovanirodriguez220@gmail.com</div>
+          </div>
+
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="map-marked-alt" />
+            </div>
+
+            <div className="text">Phx, AZ</div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
