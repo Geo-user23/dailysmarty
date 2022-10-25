59e1d9c87177a351a6e346558aea62c88d988ee3
Jovani Rodriguez <Jovanirodriguez220@gmail.com>
2022-10-24 19:05:15 -0700

    using calc and vh so page doesnt over extend

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 
+
 export default function() {
     return (
-        <div>
-            About
+        <div className="content-page-wrapper">
+            <div className='left-column'
+             
+            />
+
+            
+            <div className='right-column'>
+
+                spot for bio info
+            </div>
         </div>
-    )
+    );
 }
